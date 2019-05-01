dbe1b23dd31ca2687747c289db26866b0ed6b4b0
irtiza07 <35472116+irtiza07@users.noreply.github.com>
2019-04-30 22:16:59 -0400

    Typo fixed

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ export default function Welcome() {
     return (
         <div className="welcome-section">
             <h1 className="welcome-title"> Full Stack Developer, AI Enthusiast & Tech Addict</h1>
-            <h2 className="welcome-text"> I code simple and beautiful solutions to the most challegning problems  </h2>
+            <h2 className="welcome-text"> I code simple and beautiful solutions to the most challenging problems  </h2>
             <img className="avatar-icon" src={require('../assets/avatar.png')} alt="my-avatar"></img>
         </div>
     )
