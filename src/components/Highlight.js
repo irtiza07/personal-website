dbf002b5abe3763f178fa5d5e075b018f78a50c9
Irtiza <irtizahafiz07@gmail.com>
2019-05-19 16:15:56 -0700

    Mobile Responsive v1

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import uuid from 'uuid';
+import React from "react";
+import uuid from "uuid";
 
 export default function Highlight(props) {
-    const { icon, title, summary, frameworks, technologies } = props
-    return (
-        <div className="tech-container">
-            <img className="tech-logo" src={icon} alt="frontend-logo" ></img>
-            <h1 className="tech-title"> {title}</h1>
-            <p >{summary}</p>
-            <p className="tech-subheading">Frameworks</p>
-            {frameworks.map(value => <p key={uuid()} className="tech-list">{value}</p>)}
-            <p className="tech-subheading">Technology Stack:</p>
-            {technologies.map(value => <p key={uuid()} className="tech-list">{value}</p>)}
-        </div>
-    )
+  const { icon, title, summary, frameworks, technologies } = props;
+  return (
+    <div className="tech-container">
+      <img className="tech-logo" src={icon} alt="frontend-logo" />
+      <h1 className="tech-title"> {title}</h1>
+      <p>{summary}</p>
+      <p className="tech-subheading">Frameworks</p>
+      {frameworks.map(value => (
+        <p key={uuid()} className="tech-list">
+          {value}
+        </p>
+      ))}
+      <p className="tech-subheading">Technology</p>
+      {technologies.map(value => (
+        <p key={uuid()} className="tech-list">
+          {value}
+        </p>
+      ))}
+    </div>
+  );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,27 +1,58 @@
-import React from 'react'
-import Highlight from './Highlight';
-
+import React from "react";
+import Highlight from "./Highlight";
 
 export default function Skills() {
-    const frontendSummary = 'I like to keep the design minimalistic and use the most suited framework to achieve my goals.';
-    const frontendFrameworks = ['React JS', 'Ember JS'];
-    const frontendTechnologies = ['HTML', 'CSS', 'Bootstrap', 'Bulma', 'Responsive Mobile Design'];
+  const frontendSummary =
+    "I like to keep the design minimalistic and use the most suited framework to achieve my goals.";
+  const frontendFrameworks = ["React JS", "Ember JS"];
+  const frontendTechnologies = ["HTML", "CSS", "Bootstrap", "Bulma"];
 
-    const backendSummary = 'Primarily I make sure the code is most efficient and secure. I also keep a keen eye on readability as the project evolves.';
-    const backendFrameworks = ['Express', 'Django'];
-    const backendTechnologies = ['Python', 'Node JS', 'Java', 'RESTful APIs', 'Docker', 'PostgreSQL', 'MongoDB'];
+  const backendSummary =
+    "Primarily I make sure the code is most efficient and secure. I also keep a keen eye on readability as the project evolves.";
+  const backendFrameworks = ["Express", "Django"];
+  const backendTechnologies = [
+    "Python",
+    "Node JS",
+    "Java",
+    "RESTful APIs",
+    "Docker",
+    "PostgreSQL",
+    "MongoDB"
+  ];
 
-    const aiSummary = 'Huge interest in recent breakthroughs in Deep Learning. Worked with both predictive and classification models.';
-    const aiFrameworks = ['TensorFlow', 'Pytorch & Torch'];
-    const aiTechnologies = ['Scikit-Learn', 'Pandas', 'Matplotlib', 'Google Cloud ML Engine'];
+  const aiSummary =
+    "Huge interest in recent breakthroughs in Deep Learning. Worked with both predictive and classification models.";
+  const aiFrameworks = ["TensorFlow", "Pytorch & Torch"];
+  const aiTechnologies = [
+    "Scikit-Learn",
+    "Pandas",
+    "Matplotlib",
+    "Google Cloud ML Engine"
+  ];
 
-
-
-    return (
-        <div className="skills-container">
-            <Highlight icon={require("../assets/frontend.png")} title={"Front-end Developer"} summary={frontendSummary} frameworks={frontendFrameworks} technologies={frontendTechnologies}></Highlight>
-            <Highlight icon={require("../assets/backend.png")} title={"Back-end Developer"} summary={backendSummary} frameworks={backendFrameworks} technologies={backendTechnologies}></Highlight>
-            <Highlight icon={require("../assets/ai.png")} title={"Artificial Intelligence"} summary={aiSummary} frameworks={aiFrameworks} technologies={aiTechnologies}></Highlight>
-        </div>
-    )
+  return (
+    <div className="skills-container">
+      <Highlight
+        icon={require("../assets/frontend.png")}
+        title={"Front-end Developer"}
+        summary={frontendSummary}
+        frameworks={frontendFrameworks}
+        technologies={frontendTechnologies}
+      />
+      <Highlight
+        icon={require("../assets/backend.png")}
+        title={"Back-end Developer"}
+        summary={backendSummary}
+        frameworks={backendFrameworks}
+        technologies={backendTechnologies}
+      />
+      <Highlight
+        icon={require("../assets/ai.png")}
+        title={"Artificial Intelligence"}
+        summary={aiSummary}
+        frameworks={aiFrameworks}
+        technologies={aiTechnologies}
+      />
+    </div>
+  );
 }
